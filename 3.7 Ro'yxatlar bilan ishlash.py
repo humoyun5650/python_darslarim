69fde9f86dd6b625a1dd9b7adbd2ace486769a77
humoyun5650 <110874915+humoyun5650@users.noreply.github.com>
2022-08-09 07:46:44 +0500

    yangi fayllar qo`shildi
    test uchun fayl soxranit qildim

---
+++ 3.7 Ro'yxatlar bilan ishlash.py
@@ -0,0 +1,87 @@
+#1 Amaliyot 
+#markaz_osiyo = ["O'zbekiston", "Tojikiston", "Qirg'iziston", "Qozoqiston"]
+#print (markaz_osiyo)
+
+#2 Amaliyot 
+#print("Markaziy Osiyo davlatlari", len(markaz_osiyo)) # len bu funksiya 
+
+#3 Amaliyot
+#print("Quyidagilar MO davlatlari", sorted(markaz_osiyo))
+
+#4 Amaliyot
+#print("MO davlatlar soni:", sorted(markaz_osiyo, reverse=True), \
+#      "Eng rivojlangan MO davlati - ", markaz_osiyo[0])
+
+#5 Amaliyot 
+#print(markaz_osiyo)
+#markaz_osiyo.sort(reverse=True)
+#print(markaz_osiyo)
+
+#6Amaliyot 
+#markaz_osiyo.reverse()
+#print(markaz_osiyo)
+
+#7 Amaliyot 
+#kans = ["kitob", "qalam", "ruchka", "a"]
+#kans.sort()
+#print(kans)
+#kans.sort(reverse=True)
+#print(kans)
+
+#8 Amaliyot 
+#sonlar = list(range(0,1201))
+#print (sonlar)
+#sonlar = list(range(0,1200,2))
+#sonlar.sort()
+#print(sonlar)
+
+#9 Amaliyot
+#son2 = sonlar[0] + sonlar[2]
+#print(son2)
+
+#10 Amliyot 
+#ayir = sonlar[-1] - sonlar[0]
+#print(ayir)
+
+#11 Amaliyot
+#print("Ro`yxatdagi elemntlar soni", len(sonlar), "ta")
+
+#12 Amaliyot 
+#print("Boshidagi 20 ta qiymat quyidagilar", list(range(0,20)))
+#print(list(range(590,610)))
+#print(list(range(1180,1200)))
+
+#13 Amaliyot 
+#taomlar = ["osh", "Manti", 'shurva', "kabob", "shashlik"]
+
+#14 Amaliyot 
+#nonushta = taomlar [3:]
+#print(taomlar)
+#print(nonushta)
+
+#15 Amaliyot
+#nonushta.append("qaymoq")
+#nonushta.append("saryog'")
+
+#16 Amaliyot 
+#print(taomlar)
+#print (nonushta)
+
+#17 Amaliyot 
+#nonushta = tuple(nonushta) #tuple (o`zgarmas ro`yxat)ga aylandi 
+#print(nonushta)
+#tuple ni listga aylantirish kerak 
+#nonushta = list(nonushta) #nonushtani listga aylantirdim
+#print(nonushta)
+#nonushta[0] =('qaymoq va non') # listga ay qay va non qo`shdim
+#print(nonushta)
+#nonushta = tuple(nonushta) 
+#print(nonushta)
+
+
+
+
+ 
+
+   
+
---
+++ takrorlash.py
@@ -0,0 +1,18 @@
+ism ='Ahad'
+#familiya = 'Farhod'
+#print(ism.capitalize())
+
+oshna = "jasur", "anvar", "bobur"
+#print(oshna.capitalize())
+#print(f" {ism.lower()} {familiya.upper()}")
+
+#print(ism.upper(),"va",familiya.upper(), "qalin oshnalarim")
+#kocha=input("mahallangizni kiriting: ")
+#mahalla=input("mahalla kiriting: ")
+#viloyat=input("viloyat kiriting: ")
+#print(f"{kocha.upper()} ko'chasi, {mahalla} mahallasi, {viloyat} viloyati")
+mevalar = ['olma', 'shaftoli']
+#print(f"{ism} bozordan", mevalar[0], "olib kelish kerak")
+#print(mevalar[0].title())
+print(f" {mevalar[0].title()} va {mevalar[1].title()} mazzali")
+
